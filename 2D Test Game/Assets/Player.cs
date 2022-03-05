010e151e2669ba1423421e617a525644b88260c7
ludoteca12 <vv.mateus@hotmail.com>
2022-03-05 19:19:54 -0300

    Adding player movement

--- 2D Test Game/Assets/Player.cs
+++ 2D Test Game/Assets/Player.cs
@@ -6,7 +6,28 @@ public class Player : MonoBehaviour
 {
     private BoxCollider2D BoxCollider;
 
-    private void Start(){
+    private Vector3 moveDelta;
+
+    private void Start() {
         BoxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void FixedUpdate() {
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        // Reset moveDelta
+        moveDelta = new Vector3(x, y, 0);
+
+        // Swap sprite direction
+        if(moveDelta.x > 0)
+            transform.localScale = Vector3.one;
+        else if(moveDelta.x < 0) 
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        // Adding movement
+        transform.Translate(moveDelta * Time.deltaTime);
+        
     }
 }
