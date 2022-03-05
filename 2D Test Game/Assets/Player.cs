5e5db53690182e6d6e7e31e9442e6a2622de8f23
ludoteca12 <vv.mateus@hotmail.com>
2022-03-05 19:46:24 -0300

    Adding collision

--- 2D Test Game/Assets/Player.cs
+++ 2D Test Game/Assets/Player.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private BoxCollider2D BoxCollider;
-
     private Vector3 moveDelta;
+    private RaycastHit2D hit;
 
     private void Start() {
         BoxCollider = GetComponent<BoxCollider2D>();
@@ -26,8 +26,20 @@ public class Player : MonoBehaviour
         else if(moveDelta.x < 0) 
             transform.localScale = new Vector3(-1, 1, 1);
 
-        // Adding movement
-        transform.Translate(moveDelta * Time.deltaTime);
+        // Adding Colision, checking if the player(Actor Layer) can move in this direction
+        hit = Physics2D.BoxCast(transform.position, BoxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if(hit.collider == null) {
+            // Adding movement
+                            //  x,       y,                       z
+            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+        }
+
+        hit = Physics2D.BoxCast(transform.position, BoxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if(hit.collider == null) {
+            // Adding movement
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+        }
+
         
     }
 }
